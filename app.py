f1bcc70d4330af9112bdf33e32657de1f25b14b1
Stella Odewumi <stella@yelp.com>
2015-11-12 12:02:27 -0800

    Made base.html and home.html

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
+import jinja2
+
 from flask import Flask
 from flask import render_template
 
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def hello():
-    return render_template("index.html")
+
+    return render_template(
+	"home.html",
+	msg="hello world"
+
+    )
 
 if __name__ == "__main__":
     app.debug = True
