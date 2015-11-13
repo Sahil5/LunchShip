05e3e336f9f190d0a39ac108b13f645ea0d93d7b
Sahil Saini <ssaini@yelp.com>
2015-11-12 22:19:48 -0800

    Added is_captain logic and added logic methods for get_all_lunch_ship_presenters

--- logic.py
+++ logic.py
@@ -1,4 +1,6 @@
 from app import db
+import datetime
+import time
 
 from models.ship import Ship
 from models.crew import Crew
@@ -13,3 +15,21 @@ def create_ship(captain_id, destination, departure_time, crew):
         new_crew_member = Crew(new_ship.id, crew_member)
         db.session.add(new_crew_member)
     db.session.commit()
+
+
+def get_all_sailing_ships():
+    all_ships = db.session.query(Ship).all()
+    sailing_ships = []
+    for ship in all_ships:
+        if time.mktime(ship.departure_time.timetuple()) >= time.mktime(datetime.datetime.now().timetuple()):
+            sailing_ships.append(ship)
+    return sailing_ships
+
+
+def get_all_sailing_crews(ship_map):
+    all_crews = db.session.query(Crew).all()
+    sailing_crews = []
+    for crew in all_crews:
+        if crew.ship_id in ship_map:
+            sailing_crews.append(crew)
+    return sailing_crews
